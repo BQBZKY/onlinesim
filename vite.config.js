25d72cc6ccf2a3b4e5d21a9ced3e40dec4e118ff
BQBZKY <91907825+BQBZKY@users.noreply.github.com>
2022-10-07 02:50:37 +0300

    add path alias for src dir

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import path from 'node:path'
+
 export default defineConfig({
-  plugins: [vue({ reactivityTransform: true })],
+  plugins: [vue({reactivityTransform: true})],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
